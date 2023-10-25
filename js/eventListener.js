70cafb96741211246761da68c157e428a4907aa3
0xf000000 <Leon@margale.de>
2023-10-25 18:03:45 +0200

    okok

--- js/eventListener.js
+++ js/eventListener.js
@@ -1,8 +1,56 @@
-function setupListeners(){
-    document.querySelector('.emailButton').addEventListener("click",() => {
-        window.location = "mailto:leon@margale.de"
+
+function changeTab(clickedButton, workingName){
+
+    let active = document.querySelector('.active');
+
+   
+    active.style.color = '#ccc';
+    active.classList.remove('active');
+    
+
+    let button = document.getElementById(clickedButton);
+    button.style.color = '#44f0b6';
+    gsap.to(button, {
+        duration: 0.2, // Duration of the flash
+        backgroundColor: "#44f0b6", // Target background color
+        repeat: 1, // Repeat the animation twice
+        yoyo: true, // Reverse the animation
+        onComplete: () => {
+            button.style.backgroundColor = '#0a192f'; // Reset the background color
+        }
     });
+
+
+    
+    button.classList.add('active');
+
+
+    let tabcontent = document.getElementsByClassName('tabcontent');
+    for(let i = 0; i < tabcontent.length; i++){
+        tabcontent[i].style.display = "none";
+
+    }
+
+
+    document.getElementById(workingName).style.display = 'block';
+    
 }
 
 
-export default setupListeners;
+function setupEventlistener(){
+
+    document.querySelector('.emailButton').addEventListener("click",() => {
+        window.location = "mailto:leon@margale.de"
+    });
+    let buttons = document.getElementsByClassName("tablinks");
+
+for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", () => {
+        changeTab(buttons[i].id,buttons[i].innerText);
+    });
+
+}
+}
+
+
+export default setupEventlistener;
--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
 
 import typeStatement from "./typestatement.js";
 import handleIntersection from "./observer.js";
-import setupListeners from "./eventListener.js";
-
+import setupEventlistener from "./eventListener.js"
 // Start typing when the page loads
 setTimeout(typeStatement,300 );
 
@@ -31,25 +30,13 @@ let setupObserver = () => {
     
 }
 
-function setupTabEventlistener(){
-    let buttons = document.getElementsByClassName("tablinks");
 
-for(let i = 0; i < buttons.length; i++){
-    buttons[i].addEventListener("click", () => {
-        changeTab(buttons[i].id,buttons[i].innerText);
-    });
 
-}
-}
-
-setupListeners();
-setupTabEventlistener();
+setupEventlistener();
 setupObserver();
 
 
-document.querySelector('.emailButton').addEventListener("click",() => {
-    window.location = "mailto:leon@margale.de"
-});
+
 
 
 
@@ -60,43 +47,7 @@ document.querySelector('.emailButton').addEventListener("click",() => {
 
 
 // function to change the TABS
-function changeTab(clickedButton, workingName){
 
-    let active = document.querySelector('.active');
-
-   
-    active.style.color = '#ccc';
-    active.classList.remove('active');
-    
-
-    let button = document.getElementById(clickedButton);
-    button.style.color = '#44f0b6';
-    gsap.to(button, {
-        duration: 0.2, // Duration of the flash
-        backgroundColor: "#44f0b6", // Target background color
-        repeat: 1, // Repeat the animation twice
-        yoyo: true, // Reverse the animation
-        onComplete: () => {
-            button.style.backgroundColor = '#0a192f'; // Reset the background color
-        }
-    });
-
-
-    
-    button.classList.add('active');
-
-
-    let tabcontent = document.getElementsByClassName('tabcontent');
-    for(let i = 0; i < tabcontent.length; i++){
-        tabcontent[i].style.display = "none";
-
-    }
-
-
-    document.getElementById(workingName).style.display = 'block';
-    
-
-}
 
 
 
